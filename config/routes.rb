37c6d14e0ca81adb84886b94889892d31f868848
Chris Cenatiempo <etothe2pieye@gmail.com>
2015-11-05 09:58:08 -0700

    Updated routes and added merchant tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,54 +1,97 @@
 Rails.application.routes.draw do
-
   namespace :api do
     namespace :v1 do
-      get "/merchants/find", to: "merchants#find", defaults: {format: 'json'}
-      get "/merchants/find_all", to: "merchants#find_all", defaults: {format: 'json'}
-      get "/merchants/random", to: "merchants#random", defaults: {format: 'json'}
-      get "/merchants/:id/items", to: "merchants#items",defaults: { :format => 'json' }
-      get "/merchants/:id/invoices", to: "merchants#invoices",defaults: { :format => 'json' }
-      get "/merchants/most_items", to: "merchants#most_items",defaults: { :format => 'json' }
-      get "/merchants/:id/revenue", to: "merchants#revenue",defaults: { :format => 'json' }
-      get "/merchants/:id/favorite_customer", to: "merchants#favorite_customer",defaults: { :format => 'json' }
-      get "/merchants/:id/customers_with_pending_invoices", to: "merchants#customers_with_pending_invoices",defaults: { :format => 'json' }
-      resources :merchants, except: [:new, :edit], defaults: {format: 'json'}
+      resources :customers, defaults: { format: 'json' } do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+        end
 
-      get "/customers/find", to: "customers#find", defaults: {format: 'json'}
-      get "/customers/find_all", to: "customers#find_all", defaults: {format: 'json'}
-      get "/customers/random", to: "customers#random", defaults: {format: 'json'}
-      get "/customers/:id/invoices", to: "customers#invoices", defaults: {format: 'json'}
-      get "/customers/:id/transactions", to: "customers#transactions", defaults: {format: 'json'}
-      resources :customers, except: [:new, :edit], defaults: {format: 'json'}
+        member do
+          get :invoices
+          get :transactions
+          get :favorite_merchant
+        end
+      end
 
-      get "/items/find", to: "items#find", defaults: {format: 'json'}
-      get "/items/find_all", to: "items#find_all", defaults: {format: 'json'}
-      get "/items/random", to: "items#random", defaults: {format: 'json'}
-      get "/items/:id/invoice_items", to: "items#invoice_items", defaults: {format: 'json'}
-      get "/items/:id/merchant", to: "items#merchant", defaults: {format: 'json'}
-      resources :items, except: [:new, :edit], defaults: {format: 'json'}
+      resources :transactions, defaults: { format: 'json' } do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+        end
 
-      get "/invoices/find", to: "invoices#find", defaults: {format: 'json'}
-      get "/invoices/find_all", to: "invoices#find_all", defaults: {format: 'json'}
-      get "/invoices/random", to: "invoices#random", defaults: {format: 'json'}
-      get "/invoices/:id/transactions", to: "invoices#transactions",defaults: { :format => 'json' }
-      get "/invoices/:id/invoice_items", to: "invoices#invoice_items",defaults: { :format => 'json' }
-      get "/invoices/:id/items", to: "invoices#items",defaults: { :format => 'json' }
-      get "/invoices/:id/customer", to: "invoices#customer",defaults: { :format => 'json' }
-      get "/invoices/:id/merchant", to: "invoices#merchant",defaults: { :format => 'json' }
-      resources :invoices, except: [:new, :edit], defaults: {format: 'json'}
+        member do
+          get :invoice
+        end
+      end
 
-      get "/invoice_items/find", to: "invoice_items#find", defaults: {format: 'json'}
-      get "/invoice_items/find_all", to: "invoice_items#find_all", defaults: {format: 'json'}
-      get "/invoice_items/random", to: "invoice_items#random", defaults: {format: 'json'}
-      get "/invoice_items/:id/invoice", to: "invoice_items#invoice", defaults: {format: 'json'}
-      get "/invoice_items/:id/item", to: "invoice_items#item", defaults: {format: 'json'}
-      resources :invoice_items, except: [:new, :edit], defaults: {format: 'json'}
+      resources :items, defaults: { format: 'json' } do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+          get "most_items"
+          get "best_day"
+          get "most_revenue"
+        end
 
-      get "/transactions/find", to: "transactions#find", defaults: {format: 'json'}
-      get "/transactions/find_all", to: "transactions#find_all", defaults: {format: 'json'}
-      get "/transactions/random", to: "transactions#random", defaults: {format: 'json'}
-      get "/transactions/:id/invoice", to: "transactions#invoice", defaults: {format: 'json'}
-      resources :transactions, except: [:new, :edit], defaults: {format: 'json'}
+        member do
+          get :invoice_items
+          get :merchant
+        end
+      end
+
+      resources :merchants, defaults: { format: 'json' } do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+          get "most_revenue"
+          get "most_items"
+          get "revenue", action: "total_revenue"
+          get "most_revenue"
+        end
+
+        member do
+          get :items
+          get :invoices
+          get :revenue
+          get :items
+          get :favorite_customer
+          get :customers_with_pending_invoices
+        end
+      end
+
+      resources :invoices, defaults: { format: 'json' } do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+        end
+
+        member do
+          get :transactions
+          get :invoice_items
+          get :items
+          get :customer
+          get :merchant
+        end
+      end
+
+      resources :invoice_items, defaults: { format: 'json' } do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+        end
+
+        member do
+          get :item
+          get :invoice
+        end
+      end
     end
   end
 end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -22,4 +22,19 @@ class MerchantTest < ActiveSupport::TestCase
     assert_equal merchant.items.first.id, item1.id
     assert_equal merchant.items.last.id,  item2.id
   end
+
+  test "pending_invoice_ids pull invoice ids that are pending" do
+    successful_ids = Invoice.joins(:transactions).merge(Transaction.successful)
+                            .pluck(:invoice_id)
+    pending_ids = Invoice.all.pluck(:id) - successful_ids
+
+    assert_equal pending_ids, Merchant.pending_invoice_ids
+  end
+
+  test "successful_invoice_ids pull invoice ids that are pending" do
+    successful_ids = Invoice.joins(:transactions).merge(Transaction.successful)
+                            .pluck(:invoice_id)
+
+    assert_equal successful_ids, Merchant.successful_invoice_ids
+  end
 end
